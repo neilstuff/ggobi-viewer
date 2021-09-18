ff61dcb3301371eef8e69f75665033f2bffeb8a8
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2021-09-19 09:37:47 +1000

    Updated to latest version of electron

--- main.js
+++ main.js
@@ -2,90 +2,82 @@
 
 const config = require('./config.json');
 
-const electron = require('electron');
+const { app, protocol, BrowserWindow } = require('electron');
 
 const mime = require('mime');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
+const os = require('os')
 const pug = require('pug');
-
-const app = electron.app;
-const ipcMain = electron.ipcMain;
-const dialog = electron.dialog;
-const protocol = electron.protocol ;
-
-const nativeImage = electron.nativeImage;
-const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null
 
-function createWindow () {
+function createWindow() {
 
-  mainWindow = new BrowserWindow({width: config.mode == 'debug' ? 1600 : 1400, height: 850, 
-      resizable: true, autoHideMenuBar: true,  
-      webPreferences: {
-      }
-  });
+    mainWindow = new BrowserWindow({
+        width: config.mode == 'debug' ? 1600 : 1400,
+        height: 850,
+        resizable: true,
+        autoHideMenuBar: true
+    });
 
-  if (config.mode == "debug") {
-      mainWindow.webContents.openDevTools();
-  }
+    if (config.mode == "debug") {
+        mainWindow.webContents.openDevTools();
+    }
 
-  mainWindow.setMenu(null);
-  mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.pug'),
-      protocol: 'pug:',
-      slashes: true
-  }))
+    mainWindow.setMenu(null);
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.pug'),
+        protocol: 'pug:',
+        slashes: true
+    }))
 
-  mainWindow.on('closed', () => {
-      mainWindow = null
-  })
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
 
 }
 
-app.on('ready', function () {
+app.on('ready', function() {
 
-  protocol.registerBufferProtocol('pug', function (request, callback) {
+    protocol.registerBufferProtocol('pug', function(request, callback) {
+        let parsedUrl = require('url').parse(request.url);
+        var url = path.normalize(request.url.replace(os.type() == 'Windows_NT' ? 'pug:///' : 'pug://', '')).replace(/\?.*/g, "");
+        let ext = path.extname(url);
 
-      let parsedUrl = require('url').parse(request.url);
-      var url = path.normalize(request.url.replace('pug:///', ''));
-      let ext = path.extname(url);
+        switch (ext) {
+            case '.pug':
+                var content = pug.renderFile(url);
 
-      switch (ext) {
-          case '.pug':
-              var content = pug.renderFile(url);
+                callback({
+                    mimeType: 'text/html',
+                    data: new Buffer.from(content)
+                });
+                break;
 
-              callback({
-                  mimeType: 'text/html',
-                  data: Buffer.from(content)
-  
-              });
-              break;
+            default:
+                let output = fs.readFileSync(url);
 
-          default:
-              let output = fs.readFileSync(url);
+                return callback({ data: output, mimeType: mime.getType(ext) });
+        }
 
-              return callback({ data: output, mimeType: mime.getType(ext) });
-      }
+    });
 
-  });
-
-  createWindow();
+    createWindow();
 
 });
 
 app.on('window-all-closed', () => {
 
-  app.quit();
+    app.quit();
 
 })
 
 app.on('activate', () => {
 
-  if (mainWindow === null) {
-    createWindow();
-  }
+    if (mainWindow === null) {
+        createWindow();
+    }
 
 });
