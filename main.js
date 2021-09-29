790399bcc1224bd049b77a1e6b7204a68fbb38b5
thesourorange <Neil.Brittliff@homemail.com.au>
2021-09-29 13:46:38 +1000

    Updated Windows_NT detection

--- main.js
+++ main.js
@@ -41,25 +41,27 @@ function createWindow() {
 app.on('ready', function () {
 
   protocol.registerBufferProtocol('pug', function (request, callback) {
-      let parsedUrl = new (require('url').URL)(request.url);
-      let url = path.normalize(parsedUrl.pathname.replace('/', ''));
-      let ext = path.extname(url);
+        let parsedUrl = new (require('url').URL)(request.url);
+        let url = path.normalize(path.toNamespacedPath(parsedUrl.pathname).startsWith("\\\\?\\") ?
+                                parsedUrl.pathname.replace('/', '') :  parsedUrl.pathname);
+             
+        let ext = path.extname(url);
 
-      switch (ext) {
-          case '.pug':
-              var content = pug.renderFile(url);
+        switch (ext) {
+            case '.pug':
+                var content = pug.renderFile(url);
 
-              callback({
-                  mimeType: 'text/html',
-                  data: new Buffer.from(content)
-              });
-              break;
+                callback({
+                    mimeType: 'text/html',
+                    data: new Buffer.from(content)
+                });
+                break;
 
-          default:
-              let output = fs.readFileSync(url);
+            default:
+                let output = fs.readFileSync(url);
 
-              return callback({ data: output, mimeType: mime.getType(ext) });
-      }
+                return callback({ data: output, mimeType: mime.getType(ext) });
+        }
 
     });
 
